ac58fc2f964c16d1ae1879c1424df8dcf2de086a
João Vitor <joaovytor0@gmail.com>
2018-11-15 19:36:47 -0200

    initial commit

---
+++ hello/hello_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from hello_app import app
+
+
+class HelloTest(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_root(self):
+        assert 'Hello' in self.app.get('/').data
+
+if __name__ == '__main__':
+    unittest.main()
